0e2b20f7db59ccfdc03fe4b46000111bcd577340
John Stivers <jstivers@usc.edu>
2017-01-24 18:07:31 -0800

    push

---
+++ cmake-build-debug/CMakeFiles/hailstats.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hailstats.dir/main.cpp.o"
+  "hailstats.pdb"
+  "hailstats"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hailstats.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
